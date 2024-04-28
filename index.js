10f1dd00f71ce79306493099bb8cf1c00138d910
Mridul5300 <mridulsarkar1157@gmail.com>
2024-04-28 16:07:51 +0600

    mongodbconnection clear

--- index.js
+++ index.js
@@ -26,12 +26,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+
+    const cardCollection = client.db('cardDB').collection('card');
+
+
+    app.get('/card', async( req, res) => {
+         const cursor= cardCollection.find();
+         const result = await cursor.toArray();
+         res.send(result);
+    })
+
+   app.post('/card', async(req, res) => {
+    const AddNewSpot=req.body;
+    console.log(AddNewSpot);
+    const result = await cardCollection.insertOne(AddNewSpot);
+    res.send(result);
+   });
+
+
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
