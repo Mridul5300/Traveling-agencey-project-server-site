74a579be1d4fae4d2e23a75942fc43cd13d39b97
Mridul5300 <mridulsarkar1157@gmail.com>
2024-04-29 13:56:12 +0600

    upadte profile and dellet complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -53,6 +53,22 @@ async function run() {
     console.log(email);
     const query = {email:(email)};
     const result = await cardCollection.find(query).toArray()
+    res.send(result)
+  })
+
+  // update
+  app.get('/card/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await cardCollection.findOne(query)
+    res.send(result)
+  })
+
+  // dellete Operation
+  app.delete('/card/:id', async(req, res) => {
+    const id = req.params.id;
+    const query = { _id : new ObjectId (id)}
+    const result = await cardCollection.deleteOne(query);
     res.send(result)
   })
     
